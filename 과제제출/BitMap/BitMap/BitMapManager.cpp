cb3172d2c6691bf9b6d639786e5bd3d855cdbc1a
DoyunUm <djawleh@naver.com>
2019-10-11 18:24:57 +0900

    BitMap(Manager 추가)

---
+++ 과제제출/BitMap/BitMap/BitMapManager.cpp
@@ -0,0 +1,33 @@
+#include "BitMapManager.h"
+
+BitMapManager* BitMapManager::m_hInstence = NULL;
+
+//생성자
+BitMapManager::BitMapManager()
+{
+	m_Bit.reserve(10);
+}
+
+//초기화
+void BitMapManager::Init(HWND hWnd, HINSTANCE _hInst)
+{
+	HDC hdc = GetDC(hWnd);
+	int bit_id = 113;
+
+	BitMap* tmp_bit;
+
+	for (int i = 0; i < 9; i++)
+	{
+		tmp_bit = new BitMap;
+		tmp_bit->Init(hdc, _hInst, bit_id);
+
+		m_Bit.push_back(*tmp_bit);
+		bit_id++;
+	}
+}
+
+//소멸자
+BitMapManager::~BitMapManager()
+{
+	m_Bit.clear();
+}
