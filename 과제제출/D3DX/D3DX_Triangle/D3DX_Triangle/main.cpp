6d5859e022255a2562215971d84e147bf525d4ea
DoyunUm <djawleh@naver.com>
2020-01-17 20:38:58 +0900

    add_Orion

--- 과제제출/D3DX/D3DX_Triangle/D3DX_Triangle/main.cpp
+++ 과제제출/D3DX/D3DX_Triangle/D3DX_Triangle/main.cpp
@@ -16,7 +16,8 @@ LPDIRECT3DDEVICE9	g_pd3dDevice = NULL;	// 랜더링에 사용될 D3D 디바이스
 
 LPDIRECT3DVERTEXBUFFER9 g_FirstVB = NULL; //사각형 정점 보관
 LPDIRECT3DVERTEXBUFFER9 g_SecondVB = NULL; //삼각형 정점 보관
-LPDIRECT3DVERTEXBUFFER9 g_ThirdVB = NULL; //평행사변형 정점 보관
+LPDIRECT3DVERTEXBUFFER9 g_ThirdVB = NULL; //육각형 정점 보관
+LPDIRECT3DVERTEXBUFFER9 g_OrionVB = NULL; //육각형 정점 보관
 
 struct CUSTOMVERTEX
 {
@@ -168,6 +169,50 @@ HRESULT InitThirdVB()
 	return S_OK;
 }
 
+//오리온자리 Init
+HRESULT InitOrionVB()
+{
+	//->그리는 순서(점찍는 순서에 주의)
+	CUSTOMVERTEX vertices[] =
+	{
+		// x, y, z, rhw, color
+		{ 200.0f, 400.0f, 0.5f, 1.0f, 0xfff00000, }, //v1
+		{ 150.0f, 500.0f, 0.5f, 1.0f, 0xff0fffff, }, //v2
+		{ 140.0f, 510.0f, 0.5f, 1.0f, 0xff0fffff, }, //v3
+		{ 210.0f, 610.0f, 0.5f, 1.0f, 0xff0fffff, }, //v4
+		{ 220.0f, 640.0f, 0.5f, 1.0f, 0xff0fffff, }, //v5
+		{ 230.0f, 740.0f, 0.5f, 1.0f, 0xff0fffff, }, //v6
+		{ 180.0f, 840.0f, 0.5f, 1.0f, 0xff0fffff, }, //v7
+		{ 360.0f, 800.0f, 0.5f, 1.0f, 0xff0fffff, }, //v8
+		{ 290.0f, 730.0f, 0.5f, 1.0f, 0xff0fffff, }, //v9
+		{ 330.0f, 630.0f, 0.5f, 1.0f, 0xff0fffff, }, //v10
+		{ 280.0f, 580.0f, 0.5f, 1.0f, 0xff0fffff, }, //v11
+		{ 220.0f, 640.0f, 0.5f, 1.0f, 0xff0fffff, }, //v12
+		{ 330.0f, 630.0f, 0.5f, 1.0f, 0xff0fffff, }, //v13
+		{ 440.0f, 500.0f, 0.5f, 1.0f, 0xff0fffff, }, //v14
+		{ 480.0f, 550.0f, 0.5f, 1.0f, 0xff0fffff, }, //v15
+		{ 480.0f, 580.0f, 0.5f, 1.0f, 0xff0fffff, }, //v16
+		{ 478.0f, 600.0f, 0.5f, 1.0f, 0xff0fffff, }, //v17
+		{ 475.0f, 610.0f, 0.5f, 1.0f, 0xff0fffff, }, //v18
+	};
+
+	if (FAILED(g_pd3dDevice->CreateVertexBuffer(18 * sizeof(CUSTOMVERTEX), 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_OrionVB, NULL)))
+	{
+		return E_FAIL;
+	}
+	void* pVertices;
+
+	if (FAILED(g_OrionVB->Lock(0, sizeof(vertices), (void**)&pVertices, 0)))
+	{
+		return E_FAIL;
+	}
+
+	memcpy(pVertices, vertices, sizeof(vertices));
+	g_OrionVB->Unlock();
+
+	return S_OK;
+}
+
 HRESULT InitGeometry()
 {
 	if (FAILED(InitFirstVB()))
@@ -179,6 +224,10 @@ HRESULT InitGeometry()
 		return E_FAIL;
 	}
 	if (FAILED(InitThirdVB()))
+	{
+		return E_FAIL;
+	}
+	if (FAILED(InitOrionVB()))
 	{
 		return E_FAIL;
 	}
@@ -194,8 +243,11 @@ void Cleanup()
 {
 	//해제 순서가 중요 인터페이스 생성의 역순으로 해제하자.
 	//해체는 조립의 역순....
+	if (g_OrionVB != NULL)
+		g_OrionVB->Release();
+
 	if (g_ThirdVB != NULL)
-		g_SecondVB->Release();
+		g_ThirdVB->Release();
 
 	if (g_SecondVB != NULL)
 		g_SecondVB->Release();
@@ -208,6 +260,15 @@ void Cleanup()
 
 	if (g_pD3D != NULL)
 		g_pD3D->Release();
+}
+
+void Draw(LPDIRECT3DVERTEXBUFFER9 _vertexBuffer, _D3DPRIMITIVETYPE _type, int _figure)
+{
+	// 정점 정보가 담겨 있는 정점 버퍼를 출력 스트림으로 할당한다.
+	g_pd3dDevice->SetStreamSource(0, _vertexBuffer, 0, sizeof(CUSTOMVERTEX));
+	// D3D에게 정점 셰이더 정보를 지정한다. 대부분의 경우에는 FVF만 지정한다.
+	g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
+	g_pd3dDevice->DrawPrimitive(_type, 0, _figure);
 }
 
 /*******************************************************
@@ -226,29 +287,20 @@ void Render()
 		return;
 
 	// 후면 버퍼를 파란색(0, 0, 255)으로 지운다.
-	g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
+	g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
 	
 	// 랜더링 시작
 	if (SUCCEEDED(g_pd3dDevice->BeginScene()))
 	{
 		//실제 렌더링 명령들이 나열될 곳
-
 		//사각형
-		// 정점 정보가 담겨 있는 정점 버퍼를 출력 스트림으로 할당한다.
-		g_pd3dDevice->SetStreamSource(0, g_FirstVB, 0, sizeof(CUSTOMVERTEX));
-		// D3D에게 정점 셰이더 정보를 지정한다. 대부분의 경우에는 FVF만 지정한다.
-		g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
-		g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
-
+		Draw(g_FirstVB, D3DPT_TRIANGLESTRIP, 2);
 		//삼각형
-		g_pd3dDevice->SetStreamSource(0, g_SecondVB, 0, sizeof(CUSTOMVERTEX));
-		g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
-		g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
-
-		//
-		g_pd3dDevice->SetStreamSource(0, g_ThirdVB, 0, sizeof(CUSTOMVERTEX));
-		g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
-		g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLEFAN, 0, 6);
+		Draw(g_SecondVB, D3DPT_TRIANGLELIST, 1);
+		//육각형
+		Draw(g_ThirdVB, D3DPT_TRIANGLEFAN, 6);
+		//오리온 자리
+		Draw(g_OrionVB, D3DPT_LINESTRIP, 17);
 
 		//렌더링 종료
 		g_pd3dDevice->EndScene();
@@ -280,15 +332,12 @@ LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, INT)
 {
 	//윈도우 클래스 등록
-	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L,
-		GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
-		"D3D Triangle", NULL };
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, "D3D Triangle", NULL };
 
 	//winclass 레지스터에 등록
 	RegisterClassEx(&wc);
 
-	HWND hWnd = CreateWindow("D3D Triangle", "D3D Triangle", WS_OVERLAPPEDWINDOW, 100, 100, 1024, 768,
-		GetDesktopWindow(), NULL, NULL, wc.hInstance, NULL);
+	HWND hWnd = CreateWindow("D3D Triangle", "D3D Triangle", WS_OVERLAPPEDWINDOW, 100, 0, 1024, 1000, GetDesktopWindow(), NULL, NULL, wc.hInstance, NULL);
 
 	if (SUCCEEDED(InitD3D(hWnd)))
 	{
